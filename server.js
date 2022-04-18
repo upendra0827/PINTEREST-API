6d06420ae5026b83394c3dd2a091828fbe218a2f
Maradana Upendra <upendramaradana4@gmail.com>
2022-04-18 10:49:11 +0530

    ADDED SERVER

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const pinterestRoutes = require('./src/pinterest/routes')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.use('/api/v1/pinterest', pinterestRoutes)
+
+app.listen(port, () => {
+    console.log(`app listening on ${port}`)
+})
