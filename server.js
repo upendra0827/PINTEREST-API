3f54463be34d597e61eca61663f6697937d52593
Maradana Upendra <upendramaradana4@gmail.com>
2022-04-18 16:57:12 +0530

    updated

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ app.listen(PORT, () => {
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+  const corsWhiteList = ['http://localhost:3300/', "https://project-pinterest-api.herokuapp.com/"]
+
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  }
+  // Pass to next layer of middleware
+  next();
+});
+
+
 app.get("/", (req, res) => {
     res.send("hello world")
 })
