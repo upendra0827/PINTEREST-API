e2cfacd8d5c3210dff2178e750b6491d1305e94c
Maradana Upendra <upendramaradana4@gmail.com>
2022-04-19 12:39:44 +0530

    updated

--- src/pinterest/queries.js
+++ src/pinterest/queries.js
@@ -8,7 +8,7 @@ const getUsers = "SELECT * FROM users"
 const getUserById = "SELECT * FROM users WHERE userid=$1"
 const removeUserById = "DELETE FROM users WHERE userid = $1"
 const addUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)"
-const getUserByMail = "SELECT * FROM users WHERE email=$1"
+const getUserByMail = "SELECT * FROM users"
 
 const getCommentById = "SELECT * FROM comments where postid=$1"
 const addComments = "INSERT INTO comments (postid, data) VALUES ($1, $2)"
